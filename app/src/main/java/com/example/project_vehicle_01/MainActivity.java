3c331ce222a420ac6ca7ba29027017351a8095a7
aiswaryaais <aaishuaiswarya.98@gmail.com>
2021-11-20 11:22:55 +0530

    Update

--- app/src/main/java/com/example/project_vehicle_01/MainActivity.java
+++ app/src/main/java/com/example/project_vehicle_01/MainActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+
+import Common.IMyAidlInterface;
 
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 pager2;
     FragmentAdapter adapter;
+    IMyAidlInterface iMyAidlInterface;
+    Boolean connected=true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +38,11 @@ public class MainActivity extends AppCompatActivity {
         pager2.setAdapter(adapter);
 
 
+
         tabLayout.addTab(tabLayout.newTab().setText("Control"));
         tabLayout.addTab(tabLayout.newTab().setText("Settings"));
+
+
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -56,7 +71,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Intent intent = new Intent("com.example.project_vehicle_service_01.AIDL");
+
+        intent.setClassName("com.example.project_vehicle_service_01",
+                "com.example.project_vehicle_service_01.MyService");
+        if(getBaseContext().getApplicationContext().bindService(intent, serviceCon, Context.BIND_AUTO_CREATE)){
+            connected=true;
+            Toast.makeText(getApplicationContext(), "BindServiceSuccess", Toast.LENGTH_SHORT).show();
+        }else
+            Toast.makeText(getApplicationContext(), "BindServiceFailed", Toast.LENGTH_SHORT).show();
 
 
-    }
-    }
+}
+
+
+
+
+    private final ServiceConnection serviceCon=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            iMyAidlInterface = IMyAidlInterface.Stub.asInterface(iBinder);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
+
+
+
+}
